657e6c24177122ebb5bb7e9330a86cfb876c625e
paul <paul604@users.noreply.github.com>
2017-10-17 11:55:59 +0200

    up vue
    class to const

--- src/components/connection/VueConnection.js
+++ src/components/connection/VueConnection.js
@@ -1,62 +1,54 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
-class VueConnection extends React.Component {
-
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div >
-                {this.props.errorImg != "" ?
-                        <div>
-                            <img src={this.props.errorImg} height="250" width="300"/>
-                            <p>{this.props.errorMsg}</p>
-                        </div>
-                        :
-                        <div></div>
-                }
-                {this.props.loadImg != "" ?
+const VueConnection = function (props){
+    return (
+        <div >
+            {props.errorImg != "" ?
                     <div>
-                        <img src={this.props.loadImg}/>
+                        <img src={props.errorImg} height="250" width="300"/>
+                        <p>{props.errorMsg}</p>
                     </div>
                     :
                     <div></div>
-                }
-
-                <div id="form" >
-                    <form action="" method="post" onSubmit={ this.props.creat } >
-                        <h1>Création</h1>
-                        <hr/>
-                        <label >Nom :</label>
-                        <input id="name" type="text" name="name" onInput={ this.props.updateUserCrea }/>
-                        <br/>
-                        <label>Password :</label>
-                        <input id="password" type="password" name="password" onInput={ this.props.updateUserCrea }/>
-                        <br/>
-                        <label>image :</label>
-                        <input id="image" type="text" name="image" onInput={ this.props.updateUserCrea }/>
-                        <br/>
-                        <input type="submit" value="créer compte" />
-                    </form>
-                    <form action="" method="post" onSubmit={ this.props.connection } >
-                        <h1>Conection</h1>
-                        <hr/>
-                        <label >Nom :</label>
-                        <input id="name" type="text" name="name" onInput={ this.props.updateUserCo }/>
-                        <br/>
-                        <label>Password :</label>
-                        <input id="password" type="password" name="password" onInput={ this.props.updateUserCo }/>
-                        <br/>
-                        <input type="submit" value="Connection" />
-                    </form>
+            }
+            {props.loadImg != "" ?
+                <div>
+                    <img src={props.loadImg}/>
                 </div>
-            </div>
-        );
-    }
+                :
+                <div></div>
+            }
 
+            <div id="form" >
+                <form action="" method="post" onSubmit={ props.creat } >
+                    <h1>Création</h1>
+                    <hr/>
+                    <label >Nom :</label>
+                    <input id="name" type="text" name="name" onInput={ props.updateUserCrea }/>
+                    <br/>
+                    <label>Password :</label>
+                    <input id="password" type="password" name="password" onInput={ props.updateUserCrea }/>
+                    <br/>
+                    <label>image :</label>
+                    <input id="image" type="text" name="image" onInput={ props.updateUserCrea }/>
+                    <br/>
+                    <input type="submit" value="créer compte" />
+                </form>
+                <form action="" method="post" onSubmit={ props.connection } >
+                    <h1>Conection</h1>
+                    <hr/>
+                    <label >Nom :</label>
+                    <input id="name" type="text" name="name" onInput={ props.updateUserCo }/>
+                    <br/>
+                    <label>Password :</label>
+                    <input id="password" type="password" name="password" onInput={ props.updateUserCo }/>
+                    <br/>
+                    <input type="submit" value="Connection" />
+                </form>
+            </div>
+        </div>
+    );
 }
 
 module.exports = VueConnection;
--- src/components/home/VueHome.js
+++ src/components/home/VueHome.js
@@ -1,72 +1,64 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
-class VueHome extends React.Component {
+const VueHome = function (props){
+    return (
+        <div>
+            {props.errorImg != "" ?
+                    <div>
+                        <img src={props.errorImg} height="250" width="300"/>
+                        <p>{props.errorMsg}</p>
+                    </div>
+                    :
+                    <div></div>
+            }
 
-    constructor(props) {
-        super(props);
-    }
+            <h1>
+                home
+            </h1>
+            <div id="msg">
 
-    render() {
-        return (
-            <div>
-                {this.props.errorImg != "" ?
-                        <div>
-                            <img src={this.props.errorImg} height="250" width="300"/>
-                            <p>{this.props.errorMsg}</p>
-                        </div>
-                        :
-                        <div></div>
+                {props.loadImg != "" ?
+                    <div>
+                        <img src={props.loadImg}/>
+                    </div>
+                    :
+                    <ul id="msg_ul">
+                        {props.msg.map(function(item, index){
+                            return (
+                                <li key={index}>
+                                    <img src={item.user.image} height="50" width="50"/> {item.user.name}: ({item.date})
+                                    {item.user.id === props.dataUser.userId?
+                                         <button type="button" value={item.id} onClick={props.supMsg}>supprimer</button> :
+                                         <span/>
+                                    }
+                                    <ul>
+                                        <li>
+                                            {item.message}
+                                        </li>
+                                    </ul>
+                                    <hr/>
+                                </li>
+                            );
+                        }.bind(this))}
+                    </ul>
                 }
-
-                <h1>
-                    home
-                </h1>
-                <div id="msg">
-
-                    {this.props.loadImg != "" ?
-                        <div>
-                            <img src={this.props.loadImg}/>
-                        </div>
-                        :
-                        <ul id="msg_ul">
-                            {this.props.msg.map(function(item, index){
-                                return (
-                                    <li key={index}>
-                                        <img src={item.user.image} height="50" width="50"/> {item.user.name}: ({item.date})
-                                        {item.user.id === this.props.dataUser.userId?
-                                             <button type="button" value={item.id} onClick={this.props.supMsg}>supprimer</button> :
-                                             <span/>
-                                        }
-                                        <ul>
-                                            <li>
-                                                {item.message}
-                                            </li>
-                                        </ul>
-                                        <hr/>
-                                    </li>
-                                );
-                            }.bind(this))}
-                        </ul>
-                    }
-                </div>
-                <form onSubmit={this.props.sendMsg}>
-                    <label>Msg :</label>
-                    <textarea name="msg" value={this.props.newMsg} rows="1" cols="100" onInput={this.props.updateUserCrea} >
-                    </textarea>
-                    {this.props.newMsg === ""?
-                        <input type="submit" value="send" disabled/>
-                        :
-                        <input type="submit" value="send"/>
-                    }
-                    <input type="button" onClick={this.props.getMsg} value="F5" />
-                    <input type="button" onClick={this.props.deconection} value="déconection" />
-
-                </form>
             </div>
-        );
-    }
+            <form onSubmit={props.sendMsg}>
+                <label>Msg :</label>
+                <textarea name="msg" value={props.newMsg} rows="1" cols="100" onInput={props.updateUserCrea} >
+                </textarea>
+                {props.newMsg === ""?
+                    <input type="submit" value="send" disabled/>
+                    :
+                    <input type="submit" value="send"/>
+                }
+                <input type="button" onClick={props.getMsg} value="F5" />
+                <input type="button" onClick={props.deconection} value="déconection" />
 
+            </form>
+        </div>
+    );
 }
 
 module.exports = VueHome;
